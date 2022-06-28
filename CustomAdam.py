672b7bf1134eb274173b6a45fd3ef45c508edee7
thetechdude124 <aditya.dewan124@gmail.com>
2022-06-28 14:29:27 -0400

    Fixed Adam ImplementationRan Rosenbrock function test, and verified tensor operations.

--- CustomAdam.py
+++ CustomAdam.py
@@ -32,41 +32,42 @@ class CustomAdam(Optimizer):
         #Set loss to none
         loss = None
         #If the closure is set to True, set the loss to the closure function
-        loss = closure() if closure == None else loss
+        loss = closure() if closure != None else loss
         #Check if this is the first step - if not, increment the current step
-        if self.state["step"] == None:
+        if not self.state["step"]:
             self.state["step"] = 1
         else:
             self.state["step"] += 1
         #Iterate over "groups" of parameters (layers of parameters in the network) to begin processing and computing the next set of params
         for param_group in self.param_groups:
-            #Check if gradients have been computed for each group of parameters
-            #If not, skip over that group
-            if param_group["params"].grad.data == None:
-                continue
-            else: 
-                gradients = param_group["params"].grad.data
-            #Use Adam optimization method - first, define all the required arguments for the parameter if we are on the first step
-            if self.state["step"] == 1:
-                #Set the first and second moment estimates to zeroes
-                self.state["first_moment_estimate"] = torch.zeros_like(param_group.data)
-                self.state["second_moment_estimate"] = torch.zeros_like(param_group.data)
-            #Declare variables from state - inplace methods modify state variable directly
-            first_moment_estimate = self.state["first_moment_estimate"]
-            second_moment_estimate = self.state["second_moment_estimate"]
-            #Compute the first moment estimate - B_1 * m_t + (1-B_1) * grad (uncentered)
-            first_moment_estimate.matmul_(param_group["bias_m1"]).addmm_(gradients, 1.0 - param_group["bias_m1"])
-            #Compute the second moment estimate - B_2 * v_t + (1-B_2) * grad^2 (uncentered)
-            second_moment_estimate.matmul_(param_group["bias_m2"]).addmm_(gradients.float_power_(2.0), 1.0 - param_group["bias_m2"])
-            #Perform bias correction if parameter is set to true
-            if param_group["bias_correction"]:
-                #Perform bias correction for the first moment estimate: m_t / (1 -B_1^t)
-                first_moment_estimate.divide_(1.0 - (param_group["bias_m1"] ** self.state["step"]))
-                #Perform bias correction for second moment estimate: v_t / (1 - B_2^t)
-                second_moment_estimate.divide_(1.0 - (param_group["bias_m2"] ** self.state["step"]))
-            #Next, perform the actual update
-            #Multiply the stepsize a by the quotient of the first moment estimate and the square root of the second moment estimate plus epsilon
-            #In other words - theta = theta_{t-1} - a * first_estimate/(sqr(second_estimate) + epsilon)
-            param_group.data.add_((-param_group["stepsize"]) * first_moment_estimate.divide_(second_moment_estimate.sqrt_() + param_group["epsilon"]))
+            #Iterate over individual parameters
+            for param in param_group["params"]:
+                #Check if gradients have been computed for each parameter
+                #If not - if there are no gradients - then skip the parameter
+                if param.grad.data == None:
+                    continue
+                else: gradients = param.grad.data
+                #Use Adam optimization method - first, define all the required arguments for the parameter if we are on the first step
+                if self.state["step"] == 1:
+                    #Set the first and second moment estimates to zeroes
+                    self.state["first_moment_estimate"] = torch.zeros_like(param.data)
+                    self.state["second_moment_estimate"] = torch.zeros_like(param.data)
+                #Declare variables from state - inplace methods modify state variable directly
+                first_moment_estimate = self.state["first_moment_estimate"]
+                second_moment_estimate = self.state["second_moment_estimate"]
+                #Compute the first moment estimate - B_1 * m_t + (1-B_1) * grad (uncentered)
+                first_moment_estimate.mul_(param_group["bias_m1"]).add_(gradients * (1.0 - param_group["bias_m1"]))
+                #Compute the second moment estimate - B_2 * v_t + (1-B_2) * grad^2 (uncentered)
+                second_moment_estimate.mul_(param_group["bias_m2"]).add_(gradients.pow_(2) * (1.0 - param_group["bias_m2"]))
+                #Perform bias correction if parameter is set to true
+                if param_group["bias_correction"]:
+                    #Perform bias correction for the first moment estimate: m_t / (1 -B_1^t)
+                    first_moment_estimate.divide_(1.0 - (param_group["bias_m1"] ** self.state["step"]))
+                    #Perform bias correction for second moment estimate: v_t / (1 - B_2^t)
+                    second_moment_estimate.divide_(1.0 - (param_group["bias_m2"] ** self.state["step"]))
+                #Next, perform the actual update
+                #Multiply the stepsize a by the quotient of the first moment estimate and the square root of the second moment estimate plus epsilon
+                #In other words - theta = theta_{t-1} - a * first_estimate/(sqr(second_estimate) + epsilon)
+                param.data.add_((-param_group["stepsize"]) * first_moment_estimate.divide_(second_moment_estimate.sqrt_() + param_group["epsilon"]))
         #Return the loss
         return loss
